1be5831cec08828a515c8c16000bff75e5fce748
Mike-Griffin <mike_griffin@comcast.net>
2016-05-21 16:28:21 -0700

    Moved requests to App.create...

--- pa5/assets/javascript/script.js
+++ pa5/assets/javascript/script.js
@@ -80,6 +80,31 @@
     CreateFolder.prototype.remove = function hideCreateFolder() {
         hide('bm-create-folder-dialog');
     };
+    
+    CreateFolder.prototype.addFolderRequest = function addFolderRequest() {
+    var name = document.getElementById('create-folder-form').elements.namedItem('name').value;
+    console.log(name);
+    var description = document.getElementById('create-folder-form').elements.namedItem('description').value;
+    console.log(description);
+    if(description == '') {
+        description = "NULL";
+    }
+
+        
+    axios.post('/api/bookmarks/', {
+        name: name,
+        description: description
+    })
+    .then(function(res) {
+        console.log(res);
+    })
+    .catch(function(res){
+        console.log(res);
+    });
+        
+        
+    return false;
+}
 
     /* Mock Bookmark Service*/
     function getBookmarks() {
@@ -249,6 +274,33 @@
 
         hide('bm-create-dialog');
     };
+    
+    BookmarkCreate.prototype.addBookmarkRequest = function addBookmarkRequest() {
+        console.log("ADD BOOKMARK");
+        
+        var name = document.getElementById('create-bookmark-form').elements.namedItem('name').value;
+        var url = document.getElementById('create-bookmark-form').elements.namedItem('url').value;
+        var keywords = document.getElementById('create-bookmark-form').elements.namedItem('keywords').value;
+        var description = document.getElementById('create-bookmark-form').elements.namedItem('description').value;
+        var folderId = document.getElementById('create-bookmark-form').elements.namedItem('folderId').value;
+    
+        axios.post('/api/bookmarks/', {
+            name: name,
+            url: url,
+            keywords: keywords,
+            folderId: folderId,
+            description: description
+        })
+        .then(function(res) {
+            console.log(res);
+        })
+        .catch(function(res){
+            console.log(res);
+        }); 
+    
+        return false;
+        
+    }
 
     function BookmarkEdit() {
         this.template = App.templates['assets/templates/bm-edit-dialog.hbs'];
