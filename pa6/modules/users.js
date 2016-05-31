784dd0abbc5555a73d2bc29f2251003c7c8afb81
Kyly <kylyvass@gmail.com>
2016-05-31 12:40:15 -0700

    #58 change password done!

--- pa6/modules/users.js
+++ pa6/modules/users.js
@@ -2,20 +2,58 @@ var config = require('../config');
 var bcrypt = require('bcrypt');
 var path   = require('path');
 var Users  = require('../database/models').Users;
+
 const saltRounds = 10;
 
 module.exports.registerForm = function (req, res) {
-    res.render('login', {action: 'Register', partialName: 'users/registration-form', error: req.reportedError});
+    var error;
+    if (req.body && req.query.message === 'userExists') {
+        error = 'User name already exists please try a different user name.';
+    }
+
+    if (req.body && req.query.message === 'invalid') {
+        error = 'Invalid user name or password.';
+    }
+
+    res.render('login', {action: 'Register', partialName: 'users/registration-form', error: error});
 };
+
+module.exports.forgotPassForm = function (req, res) {
+    var error;
+    if (req.body && req.query.message === 'noMatch') {
+        error = 'Passwords given do not match. Please reenter new password.';
+    }
+
+    res.render('login', {action: 'Change Password', partialName: 'users/change-pass-form', error: error});
+};
+
+module.exports.changePassword = function(req, res) {
+    var password = req.body.password;
+    var confirm  = req.body.confirm;
+    if (password !== confirm)
+    {
+        return res.redirect('/change-password?message=noMatch');
+    }
+
+    var userId = req.session.uid;
+    if(!userId) {
+        return res.redirect('/login?message=notLoggedIn');
+    }
+
+    var update = Users.update({password: password}, {where: {id: userId}});
+    update.then(()=>res.redirect('/login?message=passChanged')).catch(()=>res.redirect('/login?message=notSure'));
+
+};
+
 
 module.exports.register = function (req, res, next) {
     var hasJavaScript = req.body.hasJavaScript !== 'false';
-    var un           = req.body.username;
-    var pw           = req.body.password;
+    var un            = req.body.username;
+    var pw            = req.body.password;
 
     if (un == "" || pw == "")
     {
-        return res.redirect('/register');
+        return res.redirect('/register?message=invalid');
     }
 
     Users.create({name: un, password: pw}).then(function (qRes) {
@@ -25,10 +63,10 @@ module.exports.register = function (req, res, next) {
         if (!user)
         {
             console.error('[register] user not defined!');
-            return res.redirect('/register');
+            return res.redirect('/register?message=invalid');
         }
 
-        req.session.uid = user.id;
+        req.session.uid  = user.id;
         req.session.user = req.body.username;
 
         if (!hasJavaScript)
@@ -40,7 +78,7 @@ module.exports.register = function (req, res, next) {
 
     }).catch(function (error) {
         console.error(error);
-        res.redirect('/register');
+        res.redirect('/register?message=userExists');
     });
 };
 
@@ -49,9 +87,8 @@ module.exports.getNewUser = function (req, res) {
         if (!user)
         {
             console.error(error);
-            return res.redirect('/register');
+            return res.redirect('/register?message=userExists');
         }
-
 
         // req.session.uid = user.userId;
         res.redirect('/bookmarks');
@@ -66,12 +103,13 @@ module.exports.login = function (req, res) {
     var un            = req.body.username;
     var pw            = req.body.password;
     var hasJavaScript = req.body.hasJavaScript !== 'false';
+    var action        = req.body.action;
 
     console.info(`User login for ${un} with password ${pw}`);
 
     if (un == "" || pw == "")
     {
-        return res.redirect('/login');
+        return res.redirect('/login?message=invalid');
     }
 
     Users.find({where: {name: un}}).then(function (user) {
@@ -79,7 +117,7 @@ module.exports.login = function (req, res) {
         if (!user)
         {
             console.error('[login] User not found');
-            return res.redirect('/login');
+            return res.redirect('/login?message=notFound');
         }
 
         if (bcrypt.compareSync(pw, user.password))
@@ -87,6 +125,10 @@ module.exports.login = function (req, res) {
             console.log('[login] Valid login');
             req.session.user = user.name;
             req.session.uid  = user.id;
+            
+            if (action === 'change-pass') {
+                return res.redirect('/change-password');
+            }
 
             if (!hasJavaScript)
             {
@@ -97,10 +139,10 @@ module.exports.login = function (req, res) {
 
         }
 
-        res.redirect('/login');
+        res.redirect('/login?message=invalid');
 
     }).catch(function (err) {
-        console.log(err);
+        return res.redirect('/login?message=notSure');
     });
 };
 
@@ -109,7 +151,28 @@ module.exports.login = function (req, res) {
  */
 module.exports.loginForm = function (req, res) {
     req.session.user = undefined;
-    res.render('login', {action: 'Login', partialName: 'users/login-form', error: req.reportedError});
+    var error;
+    if (req.body && req.query.message) {
+        switch(req.query.message) {
+            case 'notLoggedIn':
+                error = 'You must be logged in to change your password';
+                break;
+            case 'passChanged':
+                error = 'You successfully changed you password. Go ahead and try it out.';
+                break;
+            case 'invalid':
+                error = 'The password/user name you entered are invalid.';
+                break;
+            case 'notFound':
+                error = 'User not found.';
+                break;
+            case 'notSure':
+                error = 'Not sure what happened... go ahead and try to login again.';
+                break;
+        }
+    }
+
+    res.render('login', {action: 'Login', partialName: 'users/login-form', error: error});
 };
 
 /**
@@ -130,7 +193,8 @@ module.exports.auth = function (req, res, next) {
     }
     else
     {
-        if (req.url.match(/^\/api\/.*$/)) {
+        if (req.url.match(/^\/api\/.*$/))
+        {
             return res.status(401).json({message: 'Authentication required'})
         }
 
--- pa6/routes/users.js
+++ pa6/routes/users.js
@@ -8,5 +8,7 @@ router.get('/register', users.registerForm);
 router.post('/register', users.register, users.getNewUser);
 router.post('/login', users.login);
 router.get('/logout', users.logout);
+router.get('/change-password', users.forgotPassForm);
+router.post('/change-password', users.changePassword);
 
 module.exports = router;
